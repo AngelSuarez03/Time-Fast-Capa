011d73b67bd900da00c52f94a10d0b50542b63d7
mosterTeco <reyesmls11@hotmail.com>
2024-12-13 19:16:10 -0600

    actualizar status

--- src/java/dominio/ImpColaborador.java
+++ src/java/dominio/ImpColaborador.java
@@ -1,4 +1,4 @@
-    /*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -8,6 +8,7 @@ package dominio;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import mybatis.MyBatisUtil;
 import org.apache.ibatis.session.SqlSession;
 import pojo.Colaborador;
@@ -18,128 +19,161 @@ import pojo.Mensaje;
  * @author USER
  */
 public class ImpColaborador {
-    
+
     //Obtiene un lista de todos los colaboradores registrados
-    public static List<Colaborador> obtenerColaboradores(){
+    public static List<Colaborador> obtenerColaboradores() {
         SqlSession conexionBD = MyBatisUtil.obtenerConexion();
         List<Colaborador> colaboradores = null;
-        if(conexionBD != null){
+        if (conexionBD != null) {
             colaboradores = conexionBD.selectList("colaborador.obtenerColaboradores");
         }
         return colaboradores;
     }
-    
+
     //Devulve la contraseña del correo ingresado
     public static String recuperarPassword(String correo) {
         SqlSession conexionBD = MyBatisUtil.obtenerConexion();
         String password = "";
-        if(conexionBD != null){
-            password = conexionBD.selectOne("colaborador.recuperarPassword",correo);
+        if (conexionBD != null) {
+            password = conexionBD.selectOne("colaborador.recuperarPassword", correo);
         }
         return password;
     }
-    
+
     //Obtiene un lista de colaborador por el rol asignado
-    public static List<Colaborador> obtenerColaboradoresPorRol(Integer idRol){
+    public static List<Colaborador> obtenerColaboradoresPorRol(Integer idRol) {
         SqlSession conexionBD = MyBatisUtil.obtenerConexion();
         List<Colaborador> colaboradores = null;
-        if(conexionBD != null){
+        if (conexionBD != null) {
             colaboradores = conexionBD.selectList("colaborador.obtenerPorRol", idRol);
         }
         return colaboradores;
     }
-    
+
     //Obtiene un lista de colaborador sin unidad asignada
-    public static List<Colaborador> obtenerConductoresSinAsignar(){
+    public static List<Colaborador> obtenerConductoresSinAsignar() {
         SqlSession conexionBD = MyBatisUtil.obtenerConexion();
         List<Colaborador> colaboradores = null;
-        if(conexionBD != null){
+        if (conexionBD != null) {
             colaboradores = conexionBD.selectList("colaborador.noAsignados");
         }
         return colaboradores;
     }
-    
+
     //Registra un colaborador
-    public static Mensaje registrarColaborador(Colaborador colaborador){
+    public static Mensaje registrarColaborador(Colaborador colaborador) {
         Mensaje respuesta = new Mensaje();
         SqlSession conexionBD = MyBatisUtil.obtenerConexion();
-        if(conexionBD != null){
+        if (conexionBD != null) {
             try {
-                int resultado = conexionBD.insert("colaborador.registrar",colaborador);
+                int resultado = conexionBD.insert("colaborador.registrar", colaborador);
                 conexionBD.commit();
-                if(resultado > 0){
+                if (resultado > 0) {
                     respuesta.setError(false);
                     respuesta.setMensaje("Colaborador(a) registrado con éxito.");
                 } else {
                     respuesta.setError(true);
                     respuesta.setMensaje("No se pudo registrar al colaborador(a), inténtelo más tarde.");
                 }
-            } catch (Exception e){  
+            } catch (Exception e) {
                 respuesta.setError(true);
                 respuesta.setMensaje(e.getMessage());
             }
-        }else{
+        } else {
             respuesta.setError(true);
             respuesta.setMensaje("Por el momento el servicio no está disponible.");
         }
         return respuesta;
     }
-    
+
     //Editar un colaborador por id
-    public static Mensaje editarColaborador(Colaborador colaborador){
+    public static Mensaje editarColaborador(Colaborador colaborador) {
         Mensaje respuesta = new Mensaje();
         SqlSession conexionBD = MyBatisUtil.obtenerConexion();
-        if(conexionBD != null){
+        if (conexionBD != null) {
             try {
                 int editado = conexionBD.update("colaborador.editar", colaborador);
                 conexionBD.commit();
-                if(editado > 0){
+                if (editado > 0) {
                     respuesta.setError(false);
                     respuesta.setMensaje("Información del colaborador: " + colaborador.getNombre() + " a sido editada.");
-                }else {
+                } else {
                     respuesta.setError(true);
                     respuesta.setMensaje("El numero de personal ingresado no existe");
-                    
+
                 }
-                
-            } catch (Exception e){  
-                    respuesta.setError(true);
-                    respuesta.setMensaje(e.getMessage());
+
+            } catch (Exception e) {
+                respuesta.setError(true);
+                respuesta.setMensaje(e.getMessage());
             }
-        }else{
-                    respuesta.setError(true);
-                    respuesta.setMensaje("Por el momento no se puede editar la información.");
+        } else {
+            respuesta.setError(true);
+            respuesta.setMensaje("Por el momento no se puede editar la información.");
         }
         return respuesta;
     }
-    
+
     public static Mensaje eliminarColaborador(String numeroPersonal) {
         Mensaje respuesta = new Mensaje();
         SqlSession conexionBD = MyBatisUtil.obtenerConexion();
-        if(conexionBD != null){
+        if (conexionBD != null) {
             try {
-                HashMap<String,String> parametros = new LinkedHashMap<>();
+                HashMap<String, String> parametros = new LinkedHashMap<>();
                 parametros.put("numeroPersonal", numeroPersonal);
                 int editado = conexionBD.delete("colaborador.eliminar", parametros);
                 conexionBD.commit();
-                if(editado > 0){
+                if (editado > 0) {
                     respuesta.setError(false);
                     respuesta.setMensaje("Colaborador eliminado");
-                }else {
+                } else {
                     respuesta.setError(true);
                     respuesta.setMensaje("No se encontró ningun colaborador con ese N° de Personal");
-                    
+
                 }
-                
-            } catch (Exception e){  
-                    respuesta.setError(true);
-                    respuesta.setMensaje(e.getMessage());
+
+            } catch (Exception e) {
+                respuesta.setError(true);
+                respuesta.setMensaje(e.getMessage());
             }
-        }else{
-                    respuesta.setError(true);
-                    respuesta.setMensaje("Por el momento no se puede eliminar la información.");
+        } else {
+            respuesta.setError(true);
+            respuesta.setMensaje("Por el momento no se puede eliminar la información.");
         }
         return respuesta;
     }
-    
+
+    public static Mensaje editarColaboradorUnidad(int id, Integer idUnidad) {
+        Mensaje respuesta = new Mensaje();
+        SqlSession conexionBD = MyBatisUtil.obtenerConexion();
+
+        if (conexionBD != null) {
+            try {
+                Map<String, Object> params = new HashMap<>();
+                params.put("id", id);
+                params.put("idUnidad", idUnidad); 
+
+                int editado = conexionBD.update("colaborador.quitarUnidad", params);
+                conexionBD.commit();
+
+                if (editado > 0) {
+                    respuesta.setError(false);
+                    respuesta.setMensaje("Información del colaborador con la unidad " + idUnidad + " ha sido editada.");
+                } else {
+                    respuesta.setError(true);
+                    respuesta.setMensaje("El colaborador con el ID especificado no existe.");
+                }
+            } catch (Exception e) {
+                respuesta.setError(true);
+                respuesta.setMensaje("Error al editar la información: " + e.getMessage());
+            } finally {
+                conexionBD.close();
+            }
+        } else {
+            respuesta.setError(true);
+            respuesta.setMensaje("No se pudo establecer la conexión con la base de datos.");
+        }
+        return respuesta;
+    }
+
 }
--- src/java/ws/WSColaborador.java
+++ src/java/ws/WSColaborador.java
@@ -8,6 +8,7 @@ package ws;
 import com.google.gson.Gson;
 import dominio.ImpColaborador;
 import java.util.List;
+import java.util.Map;
 import javax.ws.rs.BadRequestException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -27,85 +28,105 @@ import pojo.Mensaje;
  *
  * @author USER
  */
-
 @Path("colaborador")
 public class WSColaborador {
-    
+
     @Context
     private UriInfo context;
-    
-    public WSColaborador(){
-        
+
+    public WSColaborador() {
+
     }
-    
+
     @Path("obtenerColaboradores")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Colaborador> obtenerColaborador () {
+    public List<Colaborador> obtenerColaborador() {
         return ImpColaborador.obtenerColaboradores();
     }
-    
+
     @Path("obtenerColaboradoresRol/{idRol}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Colaborador> obtenerPorRol (@PathParam("idRol") Integer idRol) {
-        if(idRol != null && idRol > 0)
+    public List<Colaborador> obtenerPorRol(@PathParam("idRol") Integer idRol) {
+        if (idRol != null && idRol > 0) {
             return ImpColaborador.obtenerColaboradoresPorRol(idRol);
+        }
         throw new BadRequestException();
-    } 
-    
+    }
+
     @Path("obtenerColaboradoresNoAsignados")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Colaborador> obtenerNoAsigados () {
+    public List<Colaborador> obtenerNoAsigados() {
         return ImpColaborador.obtenerConductoresSinAsignar();
-    } 
-    
+    }
+
     @Path("recuperarContrasenia/{correo}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String recuperarPassword (@PathParam("correo") String correo) {
-        if(correo != null){
+    public String recuperarPassword(@PathParam("correo") String correo) {
+        if (correo != null) {
             return ImpColaborador.recuperarPassword(correo);
         }
         throw new BadRequestException();
-    } 
-    
+    }
+
     @Path("registrarColaborador")
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Mensaje registrarColaborador (String jsonColaborador) {
+    public Mensaje registrarColaborador(String jsonColaborador) {
         try {
-        Gson gson = new Gson();
-        Colaborador colaborador = gson.fromJson(jsonColaborador, Colaborador.class);
-        return ImpColaborador.registrarColaborador(colaborador);
+            Gson gson = new Gson();
+            Colaborador colaborador = gson.fromJson(jsonColaborador, Colaborador.class);
+            return ImpColaborador.registrarColaborador(colaborador);
         } catch (Exception e) {
             throw new BadRequestException();
         }
-    } 
-    
+    }
+
     @Path("editarColaborador")
     @PUT
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Mensaje editarColaborador (String jsonColaborador) {
+    public Mensaje editarColaborador(String jsonColaborador) {
         try {
-        Gson gson = new Gson();
-        Colaborador colaborador = gson.fromJson(jsonColaborador, Colaborador.class);
-        return ImpColaborador.editarColaborador(colaborador);
+            Gson gson = new Gson();
+            Colaborador colaborador = gson.fromJson(jsonColaborador, Colaborador.class);
+            return ImpColaborador.editarColaborador(colaborador);
         } catch (Exception e) {
             throw new BadRequestException();
         }
-    } 
-    
+    }
+
     @Path("eliminarColaborador/{numeroPersonal}")
     @DELETE
     @Produces(MediaType.APPLICATION_JSON)
-    public Mensaje eliminarColaborador (@PathParam ("numeroPersonal") String numeroPersonal) {
-        if(numeroPersonal != null && !numeroPersonal.isEmpty())
+    public Mensaje eliminarColaborador(@PathParam("numeroPersonal") String numeroPersonal) {
+        if (numeroPersonal != null && !numeroPersonal.isEmpty()) {
             return ImpColaborador.eliminarColaborador(numeroPersonal);
+        }
         throw new BadRequestException();
-    } 
-    
+    }
+
+    @Path("editarUnidadColaborador")
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Mensaje editarUnidadColaborador(String jsonParams) {
+        try {
+            Gson gson = new Gson();
+            Map<String, Object> params = gson.fromJson(jsonParams, Map.class);
+
+            Integer id = (params.get("id") instanceof Double) ? ((Double) params.get("id")).intValue() : (Integer) params.get("id");
+            Integer idUnidad = params.get("idUnidad") == null ? null
+                    : (params.get("idUnidad") instanceof Double ? ((Double) params.get("idUnidad")).intValue() : (Integer) params.get("idUnidad"));
+
+            return ImpColaborador.editarColaboradorUnidad(id, idUnidad);
+        } catch (Exception e) {
+            throw new BadRequestException("Datos de entrada inválidos.");
+        }
+    }
+
 }
